44c5adc5c496e24c822de719d094f9def5c24488
hy3nni <ddongddi0525@naver.com>
2024-05-10 21:57:17 +0900

    Add files via upload

---
+++ day01/Quiz.java
@@ -0,0 +1,13 @@
+package day01;
+
+import java.sql.SQLOutput;
+
+public class Quiz{
+    public static void main(String[] args) {
+        System.out.println("자바수업너무 어렵다");
+        System.out.println("다음 수업은");
+        int b = 13;
+        System.out.println(b);
+
+    }
+}
---
+++ day01/Variable.java
@@ -0,0 +1,32 @@
+package day01;
+
+public class Variable {
+    public static void main(String[] args) {
+            //integer(정수)
+//        int a = 123;
+//        int b = 100;
+//        int c = 7;
+//        System.out.println(a+b);
+//
+//        //   정수형 변수  height에 유튜버의 키를 넣어주세요
+//        System.out.println("  제가 좋아하는 아이돌은 셔누이고 키는");
+//        int height = 184;
+//        System.out.println(height);
+//
+        System.out.println("지금 코스닥 지수는");
+        int height = 864;
+        System.out.println(height);
+
+        System.out.println("지금 나스닥 지수는");
+        int nasdak = 16346;
+        System.out.println(nasdak);
+
+
+        System.out.println("지금 비트코인은");
+        int bitcoin = 1000000000;
+        System.out.println(bitcoin);
+
+
+
+    }
+}
